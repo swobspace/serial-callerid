a8ffd1e8289484b0322d179f8f0101c7b5377717
Wolfgang Barth <wob@swobspace.net>
2013-06-16 14:20:47 +0200

    initial checkin

---
+++ Gemfile
@@ -0,0 +1,7 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+# gem "rails"
+gem 'serialport', '1.1.0'
+# gem 'serialport', :github => 'hparra/ruby-serialport'
+# gem 'hybridgroup-serialport', :github => 'hybridgroup/ruby-serialport'
---
+++ callerid.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+require "bundler/setup"
+require 'serialport'
+require 'yaml'
+
+CONFIG = YAML.load(File.read(File.expand_path('../config.yml', __FILE__)))
+SERIAL = CONFIG[:serial] || ( raise "no serial params set" )
+
+action_url    = CONFIG[:action_url] || "/usr/bin/chromium-browser/###callerid###"
+serial_device = SERIAL[:device]     || "/dev/ttyS0"
+baudrate      = SERIAL[:baudrate]   || 38400
+databits      = SERIAL[:databits]   || 8
+stopbits      = SERIAL[:stopbits]   || 1
+parity        = SERIAL[:parity]     || 0
+initstring    = SERIAL[:initstring] || "ATZ"
+
+SerialPort.open(serial_device, 
+                    :baudrate => baudrate,
+      		    :databits => databits,
+      		    :stopbits => stopbits,
+      		    :parity => parity ) do |s|
+
+  puts "#{s.baud} [#{s.data_bits} #{s.parity} #{s.stop_bits}]"
+  s.write "#{initstring}\r"
+
+  while line = s.readline
+    puts line
+    next unless line =~ /^RING;\d+;\d+/
+    cmd, callerid, msn = line.chomp.split(/;/)
+    puts "Callerid: #{callerid}"
+
+    system(action_url.gsub(/###callerid###/, callerid))
+    
+  end
+end
+
+
